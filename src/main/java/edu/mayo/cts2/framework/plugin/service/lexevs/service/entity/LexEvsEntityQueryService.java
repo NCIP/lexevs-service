3a45284d0354c8fed3d2ca3c35e25ead44b450e0
Kim R Frutiger <frutiger.kim@mayo.edu>
2013-03-11 14:54:24 -0500

    merge latest

--- src/main/java/edu/mayo/cts2/framework/plugin/service/lexevs/service/entity/LexEvsEntityQueryService.java
+++ src/main/java/edu/mayo/cts2/framework/plugin/service/lexevs/service/entity/LexEvsEntityQueryService.java
@@ -1,13 +1,49 @@
+/*
+* Copyright: (c) 2004-2013 Mayo Foundation for Medical Education and
+* Research (MFMER). All rights reserved. MAYO, MAYO CLINIC, and the
+* triple-shield Mayo logo are trademarks and service marks of MFMER.
+*
+* Except as contained in the copyright notice above, or as used to identify
+* MFMER as the author of this software, the trade names, trademarks, service
+* marks, or product names of the copyright holder shall not be used in
+* advertising, promotion or otherwise in connection with this software without
+* prior written authorization of the copyright holder.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 package edu.mayo.cts2.framework.plugin.service.lexevs.service.entity;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.LexGrid.LexBIG.DataModel.Collections.LocalNameList;
+import org.LexGrid.LexBIG.DataModel.Collections.ResolvedConceptReferenceList;
+import org.LexGrid.LexBIG.DataModel.Collections.SortOptionList;
+import org.LexGrid.LexBIG.DataModel.Core.CodingSchemeVersionOrTag;
 import org.LexGrid.LexBIG.DataModel.Core.ResolvedConceptReference;
+import org.LexGrid.LexBIG.DataModel.InterfaceElements.ExtensionDescription;
 import org.LexGrid.LexBIG.LexBIGService.CodedNodeSet;
+import org.LexGrid.LexBIG.LexBIGService.CodedNodeSet.PropertyType;
+import org.LexGrid.LexBIG.LexBIGService.CodedNodeSet.SearchDesignationOption;
 import org.LexGrid.LexBIG.LexBIGService.LexBIGService;
 import org.LexGrid.LexBIG.Utility.Iterators.ResolvedConceptReferencesIterator;
+import org.LexGrid.concepts.Entity;
+import org.LexGrid.concepts.Presentation;
 import org.springframework.stereotype.Component;
 
 import edu.mayo.cts2.framework.model.command.Page;
@@ -29,6 +65,10 @@ import edu.mayo.cts2.framework.model.service.core.EntityNameOrURI;
 import edu.mayo.cts2.framework.model.service.core.EntityNameOrURIList;
 import edu.mayo.cts2.framework.model.service.core.Query;
 import edu.mayo.cts2.framework.plugin.service.lexevs.service.AbstractLexEvsService;
+import edu.mayo.cts2.framework.plugin.service.lexevs.utility.PrintUtility;
+import edu.mayo.cts2.framework.service.meta.StandardModelAttributeReference;
+import edu.mayo.cts2.framework.service.profile.entitydescription.EntitiesFromAssociationsQuery;
+import edu.mayo.cts2.framework.service.profile.entitydescription.EntitiesFromAssociationsQuery.EntitiesFromAssociations;
 import edu.mayo.cts2.framework.service.profile.entitydescription.EntityDescriptionQuery;
 import edu.mayo.cts2.framework.service.profile.entitydescription.EntityDescriptionQueryService;
 
@@ -72,17 +112,40 @@ public class LexEvsEntityQueryService extends AbstractLexEvsService
 		DirectoryResult<EntityDirectoryEntry> directoryResult = null;
 		boolean atEnd = false;
 		
+		try {			
+			String codingScheme = query.getRestrictions().getCodeSystemVersion().getName();			
+			
+			CodingSchemeVersionOrTag versionOrTag = null;
+			LocalNameList entityTypes = new LocalNameList();
+			CodedNodeSet codeNodeSet = lexBigService.getNodeSet(codingScheme, versionOrTag, entityTypes);
+			System.out.println("#_#_#_#_CodeNodeSet: " + codeNodeSet.toString());
+			
+			Set<ResolvedFilter> filterSet = query.getFilterComponent();
+			for(ResolvedFilter filter : filterSet){
+				filter.getMatchValue();
+//				// PREFERRED_ONLY, NON_PREFERRED_ONLY, ALL
+				codeNodeSet.restrictToMatchingDesignations(filter.getMatchValue(), SearchDesignationOption.ALL, filter.getMatchAlgorithmReference().getContent(), null);
+			}
 		
-		try {
-			CodedNodeSet codeNodeSet = lexBigService.getNodeSet(query.getRestrictions().getCodeSystemVersion().getName(), null, null);
-			//Look at LexEVS test to see how to filter using following command
-//			codeNodeSet.restrictToMatchingDesignations(matchText, preferredOnly, matchAlgorithm, language)
-			ResolvedConceptReferencesIterator iterator = codeNodeSet.resolve(null, null, null);
-			for(ResolvedConceptReference reference : iterator.get(page.getStart(),  page.getEnd()).getResolvedConceptReference()){
-				list.add(entityTransform.transform(reference));
+			SortOptionList sortOptions = null;
+			LocalNameList propertyNames = null;
+			PropertyType [] propertyTypes = null; 
+			ResolvedConceptReferencesIterator iterator = codeNodeSet.resolve(sortOptions, propertyNames, propertyTypes);
+			
+			int start = page.getStart();
+			int end = page.getEnd();
+			System.out.println("Start: " + start + ", End: " + end);
+			ResolvedConceptReferenceList resolvedConceptReferenceList = iterator.get(start, end);
+			System.out.println("resolvedConcpetReferenceList: " + resolvedConceptReferenceList.getResolvedConceptReferenceCount());
+			ResolvedConceptReference[] resolvedConceptReferences = resolvedConceptReferenceList.getResolvedConceptReference();
+			System.out.println("resolvedConceptReferences: " + resolvedConceptReferences.length);
+			for(ResolvedConceptReference reference : resolvedConceptReferences){
+				System.out.println("ResolvedConceptReference:\n" + PrintUtility.resolvedConceptReference_toString(reference, 1));
+				EntityDirectoryEntry entry = entityTransform.transform(reference);
+				list.add(entry);
 			}
 
-			if(!iterator.hasNext()){
+			if(!iterator.hasNext()){ 
 				atEnd = true;
 			}
 			
@@ -94,17 +157,23 @@ public class LexEvsEntityQueryService extends AbstractLexEvsService
 		
 		return directoryResult;
 	}
-
+	
 	@Override
 	public Set<? extends MatchAlgorithmReference> getSupportedMatchAlgorithms() {
-		// TODO Auto-generated method stub
+		for(ExtensionDescription desc :
+			this.getLexBigService().getFilterExtensions().getExtensionDescription()){
+			
+		}
+		
 		return null;
 	}
 
 	@Override
 	public Set<? extends PropertyReference> getSupportedSearchReferences() {
-		// TODO Auto-generated method stub
-		return null;
+		PropertyReference ref = StandardModelAttributeReference.RESOURCE_SYNOPSIS.getPropertyReference();
+		// presentation
+		// resource_name - code (restricted code method)
+		return new HashSet<PropertyReference>(Arrays.asList(ref)); 
 	}
 
 	@Override
