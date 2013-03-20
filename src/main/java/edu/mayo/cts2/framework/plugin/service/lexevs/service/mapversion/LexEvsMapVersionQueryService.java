7fceb0cfee1e00e6a239f3f8029e3926472fbeef
krfrutiger <frutiger.kim@mayo.edu>
2013-03-20 17:15:27 -0500

    Added MapVersionQueryService impl

---
+++ src/main/java/edu/mayo/cts2/framework/plugin/service/lexevs/service/mapversion/LexEvsMapVersionQueryService.java
@@ -0,0 +1,425 @@
+/*
+* Copyright: (c) 2004-2013 Mayo Foundation for Medical Education and
+* Research (MFMER). All rights reserved. MAYO, MAYO CLINIC, and the
+* triple-shield Mayo logo are trademarks and service marks of MFMER.
+*
+* Except as contained in the copyright notice above, or as used to identify
+* MFMER as the author of this software, the trade names, trademarks, service
+* marks, or product names of the copyright holder shall not be used in
+* advertising, promotion or otherwise in connection with this software without
+* prior written authorization of the copyright holder.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package edu.mayo.cts2.framework.plugin.service.lexevs.service.mapversion;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import javax.annotation.Resource;
+
+import org.LexGrid.LexBIG.DataModel.Collections.CodingSchemeRenderingList;
+import org.LexGrid.LexBIG.DataModel.Core.CodingSchemeSummary;
+import org.LexGrid.LexBIG.DataModel.Core.CodingSchemeVersionOrTag;
+import org.LexGrid.LexBIG.DataModel.InterfaceElements.CodingSchemeRendering;
+import org.LexGrid.LexBIG.Exceptions.LBException;
+import org.LexGrid.LexBIG.Exceptions.LBParameterException;
+import org.LexGrid.LexBIG.Extensions.Generic.MappingExtension;
+import org.LexGrid.LexBIG.LexBIGService.LexBIGService;
+import org.LexGrid.LexBIG.Utility.Constructors;
+import org.LexGrid.codingSchemes.CodingScheme;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+import edu.mayo.cts2.framework.model.command.Page;
+import edu.mayo.cts2.framework.model.command.ResolvedFilter;
+import edu.mayo.cts2.framework.model.core.EntityReferenceList;
+import edu.mayo.cts2.framework.model.core.MatchAlgorithmReference;
+import edu.mayo.cts2.framework.model.core.PredicateReference;
+import edu.mayo.cts2.framework.model.core.PropertyReference;
+import edu.mayo.cts2.framework.model.core.SortCriteria;
+import edu.mayo.cts2.framework.model.directory.DirectoryResult;
+import edu.mayo.cts2.framework.model.entity.EntityDescription;
+import edu.mayo.cts2.framework.model.entity.EntityDirectoryEntry;
+import edu.mayo.cts2.framework.model.mapversion.MapVersion;
+import edu.mayo.cts2.framework.model.mapversion.MapVersionDirectoryEntry;
+import edu.mayo.cts2.framework.model.service.core.DocumentedNamespaceReference;
+import edu.mayo.cts2.framework.model.service.core.NameOrURI;
+import edu.mayo.cts2.framework.model.service.mapversion.types.MapRole;
+import edu.mayo.cts2.framework.model.service.mapversion.types.MapStatus;
+import edu.mayo.cts2.framework.plugin.service.lexevs.naming.CodeSystemVersionNameConverter;
+import edu.mayo.cts2.framework.plugin.service.lexevs.service.AbstractLexEvsService;
+import edu.mayo.cts2.framework.plugin.service.lexevs.utility.CommonUtils;
+import edu.mayo.cts2.framework.service.command.restriction.MapVersionQueryServiceRestrictions;
+import edu.mayo.cts2.framework.service.meta.StandardMatchAlgorithmReference;
+import edu.mayo.cts2.framework.service.meta.StandardModelAttributeReference;
+import edu.mayo.cts2.framework.service.profile.entitydescription.EntityDescriptionQuery;
+import edu.mayo.cts2.framework.service.profile.mapversion.MapVersionQuery;
+import edu.mayo.cts2.framework.service.profile.mapversion.MapVersionQueryService;
+
+/**
+ * @author <a href="mailto:frutiger.kim@mayo.edu">Kim Frutiger</a>
+ *
+ */
+@Component
+public class LexEvsMapVersionQueryService extends AbstractLexEvsService
+		implements MapVersionQueryService, InitializingBean {
+
+	public final static String SEARCH_TYPE_CONTAINS = "contains";
+	public final static String SEARCH_TYPE_EXACT_MATCH = "exactMatch";
+	public final static String SEARCH_TYPE_STARTS_WITH = "startsWith";
+	
+	public final static String ATTRIBUTE_NAME_ABOUT = "about";
+	public final static String ATTRIBUTE_NAME_RESOURCE_SYNOPSIS = "resourceSynopsis";
+	public final static String ATTRIBUTE_NAME_RESOURCE_NAME = "resourceName";
+
+	@Resource
+	private CodeSystemVersionNameConverter nameConverter;
+	
+	@Resource
+	private CodingSchemeToMapVersionTransform codingSchemeToMapVersionTransform;
+	
+	private MappingExtension mappingExtension;
+	
+	public static final String MAPPING_EXTENSION = "MappingExtension";	
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		this.mappingExtension = (MappingExtension)this.getLexBigService().getGenericExtension(MAPPING_EXTENSION);
+	}
+		
+	// ------ Local methods ----------------------
+	protected boolean validateMappingCodingScheme(String uri, String version){
+		try {
+			return this.mappingExtension.
+				isMappingCodingScheme(
+						uri, 
+						Constructors.createCodingSchemeVersionOrTagFromVersion(version));
+		} catch (LBParameterException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	
+	protected CodingSchemeRendering[] doGetResourceSummaries(
+			MapVersionQuery query, SortCriteria sortCriteria) {
+
+		Set<ResolvedFilter> filters = null; 
+		MapVersionQueryServiceRestrictions mapVersionQueryServiceRestrictions = null;
+		
+		if (query != null) {
+			mapVersionQueryServiceRestrictions = query.getRestrictions();
+			filters = query.getFilterComponent();
+		}		
+		
+		NameOrURI codeSystem = null;
+		if (mapVersionQueryServiceRestrictions != null) {
+			codeSystem = query.getRestrictions().getMap();  // TODO Correct assumption of map attribute ???
+		}
+		
+		String searchCodingSchemeName = null;
+		if (codeSystem != null) {
+			if (codeSystem.getUri() != null) {
+				searchCodingSchemeName = codeSystem.getUri();
+			} else {
+				searchCodingSchemeName = codeSystem.getName();
+			}
+		}
+		
+		LexBIGService lexBigService = getLexBigService();
+		try {
+			CodingSchemeRenderingList csrFilteredList = lexBigService.getSupportedCodingSchemes();
+			
+			// Remove any items in above returned list that are not LexEVS MappingCodeScheme type CodeSchemes 
+			csrFilteredList = filterByMappingCodeSchemes(csrFilteredList);
+			
+			if (searchCodingSchemeName != null) {
+				csrFilteredList = filterResourceSummariesByCodingSchemeName(searchCodingSchemeName, csrFilteredList);
+			}
+			
+			if (filters != null)  {
+				// Check csrFilteredList size up front and don't enter filtering legs if list is empty
+				if ((csrFilteredList != null) && (csrFilteredList.getCodingSchemeRenderingCount() > 0)) {
+					Iterator<ResolvedFilter> filtersItr = filters.iterator();
+					while (filtersItr.hasNext()) {
+						if (csrFilteredList.getCodingSchemeRenderingCount() == 0) {
+							break;
+						} else {
+							ResolvedFilter resolvedFilter = filtersItr.next();
+							csrFilteredList = filterResourceSummariesByResolvedFilter(resolvedFilter, csrFilteredList);
+						}						
+					}
+				}
+			}
+			
+			return csrFilteredList.getCodingSchemeRendering();
+		} catch(Exception e){
+			throw new RuntimeException(e);
+		}
+	}
+	
+	private CodingSchemeRenderingList filterByMappingCodeSchemes(CodingSchemeRenderingList csrFilteredList) {
+		CodingSchemeRenderingList temp = new CodingSchemeRenderingList();
+		
+		CodingSchemeRendering[] csRendering = csrFilteredList.getCodingSchemeRendering();
+		for(CodingSchemeRendering render : csRendering) {
+			CodingSchemeSummary codingSchemeSummary = render.getCodingSchemeSummary();
+			
+			String uri = codingSchemeSummary.getCodingSchemeURI();
+			String version = codingSchemeSummary.getRepresentsVersion();
+			
+			if (validateMappingCodingScheme(uri, version)) {
+				temp.addCodingSchemeRendering(render);
+			}
+		}		
+		return temp;		
+	}
+	
+	private CodingSchemeRenderingList filterResourceSummariesByCodingSchemeName(String searchCodingSchemeName, CodingSchemeRenderingList csrFilteredList) {
+		CodingSchemeRenderingList temp = new CodingSchemeRenderingList();
+		
+		CodingSchemeRendering[] csRendering = csrFilteredList.getCodingSchemeRendering();
+		for(CodingSchemeRendering render : csRendering) {
+			CodingSchemeSummary codingSchemeSummary = render.getCodingSchemeSummary();
+			if (codingSchemeSummary.getLocalName().equals(searchCodingSchemeName)) {
+				temp.addCodingSchemeRendering(render);
+			}
+		}
+		
+		return temp;
+	}
+	
+	private CodingSchemeRenderingList filterResourceSummariesByResolvedFilter(ResolvedFilter resolvedFilter, CodingSchemeRenderingList csrFilteredList) {
+		
+		boolean caseSensitive = false;
+		CodingSchemeRenderingList temp = new CodingSchemeRenderingList();
+		
+		MatchAlgorithmReference matchAlgorithmReference = resolvedFilter.getMatchAlgorithmReference();
+		String searchType = matchAlgorithmReference.getContent();
+		
+		PropertyReference propertyReference = resolvedFilter.getPropertyReference();
+		String searchAttribute = propertyReference.getReferenceTarget().getName();
+		
+		String matchStr = resolvedFilter.getMatchValue();	
+		String lowerCaseMatchStr = matchStr.toLowerCase(); // Assuming default Locale is ok to use
+		
+		CodingSchemeRendering[] csRendering = csrFilteredList.getCodingSchemeRendering();
+		for (CodingSchemeRendering render : csRendering) {
+			CodingSchemeSummary codingSchemeSummary = render.getCodingSchemeSummary();
+			if(codingSchemeSummary == null){
+				break;
+			}
+			String retrievedAttrValue = null;
+			if (searchAttribute.equals(ATTRIBUTE_NAME_ABOUT)) {
+				retrievedAttrValue = codingSchemeSummary.getCodingSchemeURI();
+			} else if (searchAttribute.equals(ATTRIBUTE_NAME_RESOURCE_SYNOPSIS)) {
+				retrievedAttrValue = codingSchemeSummary.getCodingSchemeDescription().getContent();
+			} else if (searchAttribute.equals(ATTRIBUTE_NAME_RESOURCE_NAME)) {
+				retrievedAttrValue = 
+					this.nameConverter.toCts2CodeSystemVersionName(
+						codingSchemeSummary.getLocalName(), 
+						codingSchemeSummary.getRepresentsVersion());
+			}
+			if (retrievedAttrValue != null) {
+				if (searchType.equals(SEARCH_TYPE_EXACT_MATCH)) {
+					if (caseSensitive) {
+						if (retrievedAttrValue.equals(matchStr)) {
+							temp.addCodingSchemeRendering(render);
+						}
+					} else {
+						if (retrievedAttrValue.equalsIgnoreCase(matchStr)) {
+							temp.addCodingSchemeRendering(render);
+						}						
+					}
+				} else if (searchType.equals(SEARCH_TYPE_CONTAINS)) {
+					if (caseSensitive) {
+						if (retrievedAttrValue.indexOf(matchStr) != -1) {
+							temp.addCodingSchemeRendering(render);
+						}
+					} else {
+						retrievedAttrValue = retrievedAttrValue.toLowerCase(); // Assuming default Locale is ok to use
+						if (retrievedAttrValue.indexOf(lowerCaseMatchStr) != -1) {
+							temp.addCodingSchemeRendering(render);
+						}						
+					}
+				} else if (searchType.equals(SEARCH_TYPE_STARTS_WITH)) {
+					if (caseSensitive) {
+						if (retrievedAttrValue.startsWith(matchStr)) {
+							temp.addCodingSchemeRendering(render);
+						}
+					} else {
+						retrievedAttrValue = retrievedAttrValue.toLowerCase(); // Assuming default Locale is ok to use
+						if (retrievedAttrValue.startsWith(lowerCaseMatchStr)) {
+							temp.addCodingSchemeRendering(render);
+						}						
+					}
+				}  
+			} // end brace retrievedAttr != null
+		}  // end brace for loop
+		
+		return temp;
+	}
+
+	
+	// -------- Implemented methods ----------------
+	
+	/* (non-Javadoc)
+	 * @see edu.mayo.cts2.framework.service.profile.QueryService#count(edu.mayo.cts2.framework.service.profile.ResourceQuery)
+	 */
+	@Override
+	public int count(MapVersionQuery query) {
+		return this.doGetResourceSummaries(query, null).length;
+	}
+
+	
+	/* (non-Javadoc)
+	 * @see edu.mayo.cts2.framework.service.profile.QueryService#getResourceSummaries(edu.mayo.cts2.framework.service.profile.ResourceQuery, edu.mayo.cts2.framework.model.core.SortCriteria, edu.mayo.cts2.framework.model.command.Page)
+	 */
+	@Override
+	public DirectoryResult<MapVersionDirectoryEntry> getResourceSummaries(
+			MapVersionQuery query, SortCriteria sortCriteria, Page page) {
+
+		CodingSchemeRendering[] csRendering = this.doGetResourceSummaries(query, sortCriteria);
+		CodingSchemeRendering[] csRenderingPage = CommonUtils.getRenderingPage(csRendering, page);
+
+		List<MapVersionDirectoryEntry> list = new ArrayList<MapVersionDirectoryEntry>();
+
+		for (CodingSchemeRendering render : csRenderingPage) {
+			list.add(codingSchemeToMapVersionTransform.transform(render));
+		}
+
+		boolean atEnd = (page.getEnd() >= csRendering.length) ? true : false;
+		
+		DirectoryResult<MapVersionDirectoryEntry> directoryResult = new DirectoryResult<MapVersionDirectoryEntry>(list, atEnd);
+		return directoryResult;
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.mayo.cts2.framework.service.profile.QueryService#getResourceList(edu.mayo.cts2.framework.service.profile.ResourceQuery, edu.mayo.cts2.framework.model.core.SortCriteria, edu.mayo.cts2.framework.model.command.Page)
+	 */
+	@Override
+	public DirectoryResult<MapVersion> getResourceList(MapVersionQuery query,
+			SortCriteria sortCriteria, Page page) {
+
+		CodingSchemeRendering[] csRendering = this.doGetResourceSummaries(query, sortCriteria);
+		CodingSchemeRendering[] csRenderingPage = CommonUtils.getRenderingPage(csRendering, page);
+		
+		List<MapVersion> list = new ArrayList<MapVersion>();
+
+		for (CodingSchemeRendering render : csRenderingPage) {
+			String codingSchemeName = render.getCodingSchemeSummary().getCodingSchemeURI();			
+			String version = render.getCodingSchemeSummary().getRepresentsVersion();
+			CodingSchemeVersionOrTag tagOrVersion = Constructors.createCodingSchemeVersionOrTagFromVersion(version);
+			CodingScheme codingScheme;
+			try {
+				codingScheme = this.getLexBigService().resolveCodingScheme(codingSchemeName, tagOrVersion);
+				list.add(codingSchemeToMapVersionTransform.transform(codingScheme));
+			} catch (LBException e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+		boolean atEnd = (page.getEnd() >= csRendering.length) ? true : false;
+		
+		DirectoryResult<MapVersion> directoryResult = new DirectoryResult<MapVersion>(list, atEnd);
+		return directoryResult;
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.mayo.cts2.framework.service.profile.BaseQueryService#getSupportedMatchAlgorithms()
+	 */
+	@Override
+	public Set<? extends MatchAlgorithmReference> getSupportedMatchAlgorithms() {
+
+		MatchAlgorithmReference exactMatch = StandardMatchAlgorithmReference.EXACT_MATCH.getMatchAlgorithmReference();
+		MatchAlgorithmReference contains = StandardMatchAlgorithmReference.CONTAINS.getMatchAlgorithmReference();
+		MatchAlgorithmReference startsWith = StandardMatchAlgorithmReference.STARTS_WITH.getMatchAlgorithmReference();
+
+		return new HashSet<MatchAlgorithmReference>(Arrays.asList(exactMatch,contains,startsWith));
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.mayo.cts2.framework.service.profile.BaseQueryService#getSupportedSearchReferences()
+	 */
+	@Override
+	public Set<? extends PropertyReference> getSupportedSearchReferences() {
+		
+		PropertyReference name = StandardModelAttributeReference.RESOURCE_NAME.getPropertyReference();		
+		PropertyReference about = StandardModelAttributeReference.ABOUT.getPropertyReference();	
+		PropertyReference description = StandardModelAttributeReference.RESOURCE_SYNOPSIS.getPropertyReference();
+		
+		return new HashSet<PropertyReference>(Arrays.asList(name,about,description));
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.mayo.cts2.framework.service.profile.BaseQueryService#getSupportedSortReferences()
+	 */
+	@Override
+	public Set<? extends PropertyReference> getSupportedSortReferences() {
+		return new HashSet<PropertyReference>();
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.mayo.cts2.framework.service.profile.BaseQueryService#getKnownProperties()
+	 */
+	@Override
+	public Set<PredicateReference> getKnownProperties() {
+		return new HashSet<PredicateReference>();
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.mayo.cts2.framework.service.profile.BaseService#getKnownNamespaceList()
+	 */
+	@Override
+	public List<DocumentedNamespaceReference> getKnownNamespaceList() {
+		return new ArrayList<DocumentedNamespaceReference>();
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.mayo.cts2.framework.service.profile.mapversion.MapVersionQueryService#mapVersionEntities(edu.mayo.cts2.framework.model.service.core.NameOrURI, edu.mayo.cts2.framework.model.service.mapversion.types.MapRole, edu.mayo.cts2.framework.model.service.mapversion.types.MapStatus, edu.mayo.cts2.framework.service.profile.entitydescription.EntityDescriptionQuery, edu.mayo.cts2.framework.model.core.SortCriteria, edu.mayo.cts2.framework.model.command.Page)
+	 */
+	@Override
+	public DirectoryResult<EntityDirectoryEntry> mapVersionEntities(
+			NameOrURI mapVersion, MapRole mapRole, MapStatus mapStatus,
+			EntityDescriptionQuery query, SortCriteria sort, Page page) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.mayo.cts2.framework.service.profile.mapversion.MapVersionQueryService#mapVersionEntityList(edu.mayo.cts2.framework.model.service.core.NameOrURI, edu.mayo.cts2.framework.model.service.mapversion.types.MapRole, edu.mayo.cts2.framework.model.service.mapversion.types.MapStatus, edu.mayo.cts2.framework.service.profile.entitydescription.EntityDescriptionQuery, edu.mayo.cts2.framework.model.core.SortCriteria, edu.mayo.cts2.framework.model.command.Page)
+	 */
+	@Override
+	public DirectoryResult<EntityDescription> mapVersionEntityList(
+			NameOrURI mapVersion, MapRole mapRole, MapStatus mapStatus,
+			EntityDescriptionQuery query, SortCriteria sort, Page page) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.mayo.cts2.framework.service.profile.mapversion.MapVersionQueryService#mapVersionEntityReferences(edu.mayo.cts2.framework.model.service.core.NameOrURI, edu.mayo.cts2.framework.model.service.mapversion.types.MapRole, edu.mayo.cts2.framework.model.service.mapversion.types.MapStatus, edu.mayo.cts2.framework.service.profile.entitydescription.EntityDescriptionQuery, edu.mayo.cts2.framework.model.core.SortCriteria, edu.mayo.cts2.framework.model.command.Page)
+	 */
+	@Override
+	public EntityReferenceList mapVersionEntityReferences(NameOrURI mapVersion,
+			MapRole mapRole, MapStatus mapStatus, EntityDescriptionQuery query,
+			SortCriteria sort, Page page) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
