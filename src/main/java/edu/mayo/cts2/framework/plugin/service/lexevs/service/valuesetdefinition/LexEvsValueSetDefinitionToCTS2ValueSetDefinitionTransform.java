02e9a7b96405e9baebfb4a33cb2be72b646e7d6f
Kevin Peterson <kevin.peterson@mayo.edu>
2013-04-23 09:02:57 -0500

    Updated the ValueSetDefintion Services.

--- src/main/java/edu/mayo/cts2/framework/plugin/service/lexevs/service/valuesetdefinition/LexEvsValueSetDefinitionToCTS2ValueSetDefinitionTransform.java
+++ src/main/java/edu/mayo/cts2/framework/plugin/service/lexevs/service/valuesetdefinition/LexEvsValueSetDefinitionToCTS2ValueSetDefinitionTransform.java
@@ -23,13 +23,30 @@
 */
 package edu.mayo.cts2.framework.plugin.service.lexevs.service.valuesetdefinition;
 
-import org.LexGrid.codingSchemes.CodingScheme;
-import org.lexgrid.valuesets.dto.ResolvedValueSetCodedNodeSet;
+import org.LexGrid.valueSets.CodingSchemeReference;
+import org.LexGrid.valueSets.DefinitionEntry;
+import org.LexGrid.valueSets.EntityReference;
+import org.LexGrid.valueSets.PropertyReference;
+import org.LexGrid.valueSets.ValueSetDefinitionReference;
+import org.LexGrid.valueSets.types.DefinitionOperator;
+import org.springframework.stereotype.Component;
 
+import edu.mayo.cts2.framework.model.core.PredicateReference;
+import edu.mayo.cts2.framework.model.core.URIAndEntityName;
 import edu.mayo.cts2.framework.model.core.ValueSetReference;
+import edu.mayo.cts2.framework.model.core.types.AssociationDirection;
+import edu.mayo.cts2.framework.model.core.types.SetOperator;
+import edu.mayo.cts2.framework.model.valuesetdefinition.AssociatedEntitiesReference;
+import edu.mayo.cts2.framework.model.valuesetdefinition.CompleteCodeSystemReference;
+import edu.mayo.cts2.framework.model.valuesetdefinition.CompleteValueSetReference;
+import edu.mayo.cts2.framework.model.valuesetdefinition.PropertyQueryReference;
+import edu.mayo.cts2.framework.model.valuesetdefinition.SpecificEntityList;
 import edu.mayo.cts2.framework.model.valuesetdefinition.ValueSetDefinition;
 import edu.mayo.cts2.framework.model.valuesetdefinition.ValueSetDefinitionDirectoryEntry;
-import edu.mayo.cts2.framework.plugin.service.lexevs.transform.LexEvsToCTS2Transformer;
+import edu.mayo.cts2.framework.model.valuesetdefinition.ValueSetDefinitionEntry;
+import edu.mayo.cts2.framework.model.valuesetdefinition.types.LeafOrAll;
+import edu.mayo.cts2.framework.model.valuesetdefinition.types.TransitiveClosure;
+import edu.mayo.cts2.framework.plugin.service.lexevs.transform.AbstractBaseTransform;
 
 /**
  * Transforms a LexEVS ValueSetDefinition object into a CTS2 ValueSetDefinition object.
@@ -37,60 +54,149 @@ import edu.mayo.cts2.framework.plugin.service.lexevs.transform.LexEvsToCTS2Trans
  * @author <a href="mailto:frutiger.kim@mayo.edu">Kim Frutiger</a>
  *
  */
-public class LexEvsValueSetDefinitionToCTS2ValueSetDefinitionTransform implements LexEvsToCTS2Transformer <ValueSetDefinition, CodingScheme, ValueSetDefinitionDirectoryEntry, ResolvedValueSetCodedNodeSet> {
+@Component
+public class LexEvsValueSetDefinitionToCTS2ValueSetDefinitionTransform 
+	extends AbstractBaseTransform<
+		ValueSetDefinition, 
+		org.LexGrid.valueSets.ValueSetDefinition, 
+		ValueSetDefinitionDirectoryEntry, 
+		org.LexGrid.valueSets.ValueSetDefinition> {
 
-	public ValueSetDefinition transformToValueSetDefinition(org.LexGrid.valueSets.ValueSetDefinition lexEvsVSD) {
-		
-		ValueSetDefinition cts2VSD = new ValueSetDefinition();
+	public ValueSetDefinition transformFullDescription(org.LexGrid.valueSets.ValueSetDefinition lexEvsVSD) {
 		if (lexEvsVSD == null) {
-			return cts2VSD;
+			return null;
 		}
 		
+		ValueSetDefinition cts2VSD = new ValueSetDefinition();
+
 		cts2VSD.setAbout(lexEvsVSD.getValueSetDefinitionURI());
-		cts2VSD.setFormalName(lexEvsVSD.getValueSetDefinitionURI());  // TODO Ok mapping???
-		
-		// TODO Need to transform LexEVS Definition Entry objects into CTS2 ValueSetDefinitionEntry objects ???
-//		ValueSetDefinitionEntry[] vEntryList;
-//		cts2VSD.setEntry(vEntryList);
-		
+		cts2VSD.setFormalName(lexEvsVSD.getValueSetDefinitionURI());
+
 		ValueSetReference vsReference = new ValueSetReference();
 		vsReference.setContent(lexEvsVSD.getValueSetDefinitionURI());
 		cts2VSD.setDefinedValueSet(vsReference);
 		
-		// TODO status mapping ???
-		lexEvsVSD.getStatus();
-		
-		// TODO owner mapping
-		lexEvsVSD.getOwner();
-		
-		// TODO effective date mapping
-		lexEvsVSD.getEffectiveDate();
-		
-		
-		// TODO source mapping - need to map org.LexGrid.commonTypes.Source entries into CTS2 TBD objects ???
-		lexEvsVSD.getSource();
-				
-		//return cts2VSD;
-		
-		throw new UnsupportedOperationException("Transform of LexEVS ValueSetDefinition into CTS2 ValueSetDefinition is under construction");		
+		for(DefinitionEntry entry : lexEvsVSD.getDefinitionEntry()){
+			ValueSetDefinitionEntry cts2Entry = new ValueSetDefinitionEntry();
+			
+			if(entry.getCodingSchemeReference() != null){
+				this.add(entry.getCodingSchemeReference(), cts2Entry);
+			}
+			if(entry.getEntityReference() != null){
+				this.add(entry.getEntityReference(), cts2Entry);
+			}
+			if(entry.getPropertyReference() != null){
+				this.add(entry.getPropertyReference(), cts2Entry);
+			}
+			if(entry.getPropertyReference() != null){
+				this.add(entry.getValueSetDefinitionReference(), cts2Entry);
+			}
+			
+			cts2Entry.setEntryOrder(entry.getRuleOrder());
+			cts2Entry.setOperator(this.toSetOperator(entry.getOperator()));
+		}
+	
+		return cts2VSD;
 	}
 	
-	// TODO Not sure if method is needed/useful. May be better to transform directly in LexValueSetDefinitionQueryService
-	@Override
-	public ValueSetDefinitionDirectoryEntry transformSummaryDescription(ResolvedValueSetCodedNodeSet resolvedValueSetCodedNodeSet) {
-//		ValueSetDefinitionDirectoryEntry vsdDirEntry = new ValueSetDefinitionDirectoryEntry();
+	private SetOperator toSetOperator(DefinitionOperator operator){
+		switch(operator) {
+			case OR : return SetOperator.UNION;
+			case AND : return SetOperator.INTERSECT;
+			case SUBTRACT : return SetOperator.SUBTRACT;
+			default : throw new IllegalStateException();
+		}
+	}
+	
+	private void add(
+			ValueSetDefinitionReference valueSetDefinitionReference,
+			ValueSetDefinitionEntry cts2Entry) {
+		CompleteValueSetReference vsr = new CompleteValueSetReference();
 		
-		//return vsdDirEntry;
-		throw new UnsupportedOperationException("Transform of LexEVS ValueSetDefinition into CTS2 ValueSetDefinitionDirectoryEntry is under construction");		
+		cts2Entry.setCompleteValueSet(vsr);
 	}
 
-	/* (non-Javadoc)
-	 * @see edu.mayo.cts2.framework.plugin.service.lexevs.utility.LexEvsToCTS2Transformer#transformDescription(java.lang.Object)
-	 */
+	private void add(
+			PropertyReference propertyReference,
+			ValueSetDefinitionEntry cts2Entry) {
+		PropertyQueryReference pqr = new PropertyQueryReference();
+		
+		cts2Entry.setPropertyQuery(pqr);
+	}
+
+	private void add(
+			EntityReference entityReference,
+			ValueSetDefinitionEntry cts2Entry) {
+		URIAndEntityName uriAndName = new URIAndEntityName();
+		uriAndName.setName(entityReference.getEntityCode());
+		uriAndName.setNamespace(entityReference.getEntityCodeNamespace());
+		
+		if(entityReference.getReferenceAssociation() != null){
+			AssociatedEntitiesReference ref = new AssociatedEntitiesReference();
+			ref.setReferencedEntity(uriAndName);
+			ref.setDirection(
+				entityReference.getTargetToSource() ? 
+					AssociationDirection.TARGET_TO_SOURCE : 
+					AssociationDirection.SOURCE_TO_TARGET);
+			
+			ref.setTransitivity(
+					entityReference.getTransitiveClosure() ? 
+						TransitiveClosure.TRANSITIVE_CLOSURE : 
+						TransitiveClosure.DIRECTLY_ASSOCIATED);
+			
+			ref.setLeafOnly(
+					entityReference.getLeafOnly() ?
+						LeafOrAll.LEAF_ONLY :
+						LeafOrAll.ALL_INTERMEDIATE_NODES);
+			
+			PredicateReference predicate = new PredicateReference();
+			predicate.setName(entityReference.getReferenceAssociation());
+			
+			//namespace for a predicate is not guaranteed in LexEVS... not sure 
+			//how we want to handle this. For now, assume the same namespace
+			//as the starting entity.
+			predicate.setNamespace(entityReference.getEntityCodeNamespace());
+			ref.setPredicate(predicate);
+			
+			cts2Entry.setAssociatedEntities(ref);
+		} else {
+			SpecificEntityList sel = new SpecificEntityList();
+			
+			sel.addReferencedEntity(uriAndName);
+			
+			cts2Entry.setEntityList(sel);
+		}
+	}
+
+	private void add(
+			CodingSchemeReference codingSchemeReference,
+			ValueSetDefinitionEntry cts2Entry) {
+		CompleteCodeSystemReference ccr = new CompleteCodeSystemReference();
+
+		String codeSystemName = codingSchemeReference.getCodingScheme();
+		
+		ccr.setCodeSystem(
+			this.getTransformUtils().toCodeSystemReference(codeSystemName, null));
+		
+		cts2Entry.setCompleteCodeSystem(ccr);
+	}
+
 	@Override
-	public ValueSetDefinition transformFullDescription(CodingScheme data) {
-		// TODO Auto-generated method stub
-		return null;
+	public ValueSetDefinitionDirectoryEntry transformSummaryDescription(org.LexGrid.valueSets.ValueSetDefinition lexEvsVSD) {
+		if (lexEvsVSD == null) {
+			return null;
+		}
+		
+		ValueSetDefinitionDirectoryEntry vsdDirEntry = new ValueSetDefinitionDirectoryEntry();
+		
+		vsdDirEntry.setAbout(lexEvsVSD.getValueSetDefinitionURI());
+		vsdDirEntry.setFormalName(lexEvsVSD.getValueSetDefinitionURI());
+
+		ValueSetReference vsReference = new ValueSetReference();
+		vsReference.setContent(lexEvsVSD.getValueSetDefinitionURI());
+		vsdDirEntry.setDefinedValueSet(vsReference);
+		
+		return vsdDirEntry;
 	}
 	
 }
