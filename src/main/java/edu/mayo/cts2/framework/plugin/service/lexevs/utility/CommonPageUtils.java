e0b6fe91e07eb317cc3cf944b70f0b3b2662cdf4
krfrutiger <frutiger.kim@mayo.edu>
2013-05-01 11:10:42 -0500

    Removed method no longer needed and fixed method return value to be templated array vs Object array

--- src/main/java/edu/mayo/cts2/framework/plugin/service/lexevs/utility/CommonPageUtils.java
+++ src/main/java/edu/mayo/cts2/framework/plugin/service/lexevs/utility/CommonPageUtils.java
@@ -35,7 +35,6 @@ import org.LexGrid.LexBIG.Exceptions.LBResourceUnavailableException;
 import org.LexGrid.LexBIG.LexBIGService.CodedNodeSet;
 import org.LexGrid.LexBIG.LexBIGService.LexBIGService;
 import org.LexGrid.LexBIG.Utility.Iterators.ResolvedConceptReferencesIterator;
-import org.LexGrid.codingSchemes.CodingScheme;
 
 import edu.mayo.cts2.framework.model.command.Page;
 import edu.mayo.cts2.framework.model.core.SortCriteria;
@@ -126,14 +125,8 @@ public final class CommonPageUtils {
 	}
 	
 
-	
-	public static <T> Object[] getRenderingPage(List<T> list, Page page){
-		Object [] renderedArray = list.toArray(new CodingScheme[0]);
-		return CommonPageUtils.getPage(renderedArray, page);
-	}
-	
 	@SuppressWarnings("unchecked")
-	public static <T> Object[] getPage(T[] data, Page page) {
+	public static <T> T[] getPage(T[] data, Page page) {
 		int start = page.getStart();
 		int end = page.getEnd();
 	    T typeVar = null;
