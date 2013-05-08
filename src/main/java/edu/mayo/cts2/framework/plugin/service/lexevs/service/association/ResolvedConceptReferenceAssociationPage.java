ad7a8b6f397d04a7b98d9373cd1ca45e1be1fab5
Kevin Peterson <kevin.peterson@mayo.edu>
2013-05-08 16:02:19 -0500

    Added ResolvedConceptReferenceAssociationPage.

---
+++ src/main/java/edu/mayo/cts2/framework/plugin/service/lexevs/service/association/ResolvedConceptReferenceAssociationPage.java
@@ -0,0 +1,59 @@
+package edu.mayo.cts2.framework.plugin.service.lexevs.service.association;
+
+import org.LexGrid.LexBIG.DataModel.Core.ResolvedConceptReference;
+
+public class ResolvedConceptReferenceAssociationPage {
+	
+	protected enum Direction {SOURCEOF, TARGETOF}
+	
+	private Direction direction;
+	
+	private int start;
+	
+	private int end;
+	
+	private ResolvedConceptReference resolvedConceptReference;
+
+	protected ResolvedConceptReferenceAssociationPage(Direction direction,
+			int start, int end,
+			ResolvedConceptReference resolvedConceptReference) {
+		super();
+		this.direction = direction;
+		this.start = start;
+		this.end = end;
+		this.resolvedConceptReference = resolvedConceptReference;
+	}
+
+	public ResolvedConceptReference getResolvedConceptReference() {
+		return resolvedConceptReference;
+	}
+
+	public void setResolvedConceptReference(ResolvedConceptReference resolvedConceptReference) {
+		this.resolvedConceptReference = resolvedConceptReference;
+	}
+
+	public int getStart() {
+		return start;
+	}
+
+	public void setStart(int start) {
+		this.start = start;
+	}
+
+	public int getEnd() {
+		return end;
+	}
+
+	public void setEnd(int end) {
+		this.end = end;
+	}
+
+	public Direction getDirection() {
+		return direction;
+	}
+
+	public void setDirection(Direction direction) {
+		this.direction = direction;
+	}
+
+}
